d8d2f996c6ab91eb9ff66aa1d3049db47b5f227a
Jiaying330 <jiaying.hou330@gmail.com>
2024-07-15 17:58:09 -0400

    add random feature

--- MakeItStick/src/components/Home.jsx
+++ MakeItStick/src/components/Home.jsx
@@ -1,66 +1,84 @@
 import React, { useState, useEffect } from "react";
 import "./Home.scss";
-import problems from "../../public/problems.txt";
-import network_problems from "../../public/network_problems.txt";
-import html_problems from "../../public/html_problems.txt";
-import css_problems from "../../public/css_problems.txt";
+import problems from "../problems/problems.txt";
+import network_problems from "../problems/network_problems.txt";
+import html_problems from "../problems/html_problems.txt";
+import css_problems from "../problems/css_problems.txt";
+import js_1_problems from "../problems/js-1_problems.txt";
 import Card from "./Card";
 import SelectButton from "./SelectButton";
-// import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
-// import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 
 export default function Home() {
   const [questions, setQuestions] = useState([]);
   const [network, setNetwork] = useState(false);
   const [html, setHTML] = useState(false);
   const [css, setCSS] = useState(false);
-  // const [network_questions, setNetworkQuestions] = useState([]);
-  // const [html_questions, setHTMLQuestions] = useState([]);
-  // const [css_questions, setCSSQuestions] = useState([]);
+  const [js1, setJS1] = useState(false);
+  const [random, setRandom] = useState(false);
+  const [indices, setIndices] = useState([]);
   const [currIndex, setCurrIndex] = useState(0);
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    // fetchProblems(problems, setQuestions);
+    reset();
+    setQuestions([]);
     if (network) {
-      fetchProblems(network_problems, setQuestions);
-    } else if (html) {
-      fetchProblems(html_problems, setQuestions);
-    } else if (css) {
-      fetchProblems(css_problems, setQuestions);
+      fetchProblems(network_problems, setQuestions, random);
     }
-  }, [network, html, css]);
+    if (html) {
+      fetchProblems(html_problems, setQuestions, random);
+    }
+    if (css) {
+      fetchProblems(css_problems, setQuestions, random);
+    }
+    if (js1) {
+      fetchProblems(js_1_problems, setQuestions, random);
+    }
+  }, [network, html, css, js1, random]);
 
   function nextQuestionOnClick() {
     if (count < questions.length) {
+      // let index = random ? Math.floor(Math.random() * (questions.length - 1)) : currIndex;
       setCurrIndex((prev) => prev + 1);
+      // setCurrIndex(index);
       setCount((prev) => prev + 1);
-      //   questions.shift();
     }
     // const nextIndex = Math.floor(Math.random() * (questions.length - 1));
   }
 
+  function reset() {
+    setCount(0);
+    setCurrIndex(0);
+    // setNetwork(false);
+    // setCSS(false);
+    // setHTML(false);
+    // setJS1(false);
+  }
   function selectOnClick(event) {
     const id = event.target.id;
+    reset();
     if (id == "network") {
-      // const nextVal = !network;
-      // console.log(nextVal);
-      setNetwork(true);
-      setCSS(false);
-      setHTML(false);
+      // reset();
+      setNetwork((prev) => !prev);
     } else if (id == "css") {
-      // const nextVal = !network;
-      // console.log(nextVal);
-      setCSS(true);
-      setNetwork(false);
-      setHTML(false);
-    } else {
-      // const nextVal = !network;
-      // console.log(nextVal);
-      setHTML(true);
-      setCSS(false);
-      setNetwork(false);
+      // reset();
+      setCSS((prev) => !prev);
+    } else if (id == "html") {
+      // reset();
+      setHTML((prev) => !prev);
+    } else if (id == "javascript-1") {
+      // reset();
+      setJS1((prev) => !prev);
     }
+  }
+  function randomOnClick() {
+    // const nextValue = !random;
+    // if (!nextValue) {
+
+    // } else {
+
+    // }
+    setRandom((prev) => !prev);
   }
 
   return (
@@ -74,6 +92,15 @@ export default function Home() {
           />
           <SelectButton name="HTML" active={html} onClick={selectOnClick} />
           <SelectButton name="CSS" active={css} onClick={selectOnClick} />
+          <SelectButton
+            name="JavaScript-1"
+            active={js1}
+            onClick={selectOnClick}
+          />
+          <SelectButton name="Random" active={random} onClick={randomOnClick} />
+          {/* <button className="main__button main__button-randomize">
+            Randomize
+          </button> */}
         </div>
         <div className="main__count">
           Problems Solved: {count} / {questions.length}
@@ -87,13 +114,21 @@ export default function Home() {
   );
 }
 
-function fetchProblems(path, setFunction) {
+function fetchProblems(path, setFunction, random) {
   fetch(path)
     .then((res) => res.text())
     .then((res) => {
       const q = res.split("\n");
-      // setCSSQuestions(q);
-      // console.log("in function, q = ", q);
-      setFunction(q);
+      setFunction((prev) => {
+        return random ? shuffleArray([...prev, ...q]) : [...prev, ...q];
+      });
     });
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
