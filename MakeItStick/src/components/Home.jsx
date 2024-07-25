4cadf48fd638716ba52dba781c33aadca71ddd10
Jiaying330 <jiaying.hou330@gmail.com>
2024-07-25 12:40:10 -0700

    change problem fetching logic, add local storage

--- MakeItStick/src/components/Home.jsx
+++ MakeItStick/src/components/Home.jsx
@@ -1,44 +1,40 @@
 import React, { useState, useEffect } from "react";
 import "./Home.scss";
-import problems from "../problems/problems.txt";
-import network_problems from "../problems/network_problems.txt";
-import html_problems from "../problems/html_problems.txt";
-import css_problems from "../problems/css_problems.txt";
-import js_1_problems from "../problems/js-1_problems.txt";
-import js_2_problems from "../problems/js-2_problems.txt";
 import Card from "./Card";
 import SelectButton from "./SelectButton";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+import { problems } from "../problems/problems";
 
 export default function Home() {
   const [questions, setQuestions] = useState([]);
+  const [allQuestions, setAllQuestions] = useState(problems);
   const [network, setNetwork] = useState(false);
   const [html, setHTML] = useState(false);
   const [css, setCSS] = useState(false);
-  const [js1, setJS1] = useState(false);
-  const [js2, setJS2] = useState(false);
+  const [javascript, setJavascript] = useState(false);
   const [random, setRandom] = useState(false);
   const [currIndex, setCurrIndex] = useState(0);
   const [count, setCount] = useState(0);
-
+  const [savedQuestions, setSavedQuestions] = useLocalStorage(
+    "savedQuestions",
+    []
+  );
   useEffect(() => {
     reset();
     setQuestions([]);
     if (network) {
-      fetchProblems(network_problems, setQuestions, random);
+      setProblems(setQuestions, "network", random, allQuestions);
     }
     if (html) {
-      fetchProblems(html_problems, setQuestions, random);
+      setProblems(setQuestions, "html", random, allQuestions);
     }
     if (css) {
-      fetchProblems(css_problems, setQuestions, random);
+      setProblems(setQuestions, "css", random, allQuestions);
     }
-    if (js1) {
-      fetchProblems(js_1_problems, setQuestions, random);
+    if (javascript) {
+      setProblems(setQuestions, "javascript", random, allQuestions);
     }
-    if (js2) {
-      fetchProblems(js_2_problems, setQuestions, random);
-    }
-  }, [network, html, css, js1, js2, random]);
+  }, [network, html, css, javascript, random]);
 
   function prevQuestionOnClick() {
     if (count > 0) {
@@ -57,6 +53,7 @@ export default function Home() {
     setCount(0);
     setCurrIndex(0);
   }
+
   function selectOnClick(event) {
     const id = event.target.id;
     reset();
@@ -66,14 +63,11 @@ export default function Home() {
       setCSS((prev) => !prev);
     } else if (id == "html") {
       setHTML((prev) => !prev);
-    } else if (id == "javascript-1") {
-      setJS1((prev) => !prev);
-    } else if (id == "javascript-2") {
-      setJS2((prev) => !prev);
+    } else if (id == "javascript") {
+      setJavascript((prev) => !prev);
+    } else if (id == "random") {
+      setRandom((prev) => !prev);
     }
-  }
-  function randomOnClick() {
-    setRandom((prev) => !prev);
   }
 
   return (
@@ -88,16 +82,11 @@ export default function Home() {
           <SelectButton name="HTML" active={html} onClick={selectOnClick} />
           <SelectButton name="CSS" active={css} onClick={selectOnClick} />
           <SelectButton
-            name="JavaScript-1"
-            active={js1}
+            name="JavaScript"
+            active={javascript}
             onClick={selectOnClick}
           />
-          <SelectButton
-            name="JavaScript-2"
-            active={js2}
-            onClick={selectOnClick}
-          />
-          <SelectButton name="Random" active={random} onClick={randomOnClick} />
+          <SelectButton name="Random" active={random} onClick={selectOnClick} />
         </div>
         <div className="main__count">
           Problems Solved: {count} / {questions.length}
@@ -120,15 +109,14 @@ export default function Home() {
   );
 }
 
-function fetchProblems(path, setFunction, random) {
-  fetch(path)
-    .then((res) => res.text())
-    .then((res) => {
-      const q = res.split("\n");
-      setFunction((prev) => {
-        return random ? shuffleArray([...prev, ...q]) : [...prev, ...q];
-      });
-    });
+function setProblems(setFunction, type, random, allQuestions) {
+  let questions = allQuestions.filter((q) => q.type == type);
+  questions = questions.map((q) => q.problem);
+  setFunction((prev) => {
+    return random
+      ? shuffleArray([...prev, ...questions])
+      : [...prev, ...questions];
+  });
 }
 
 function shuffleArray(array) {
---
+++ MakeItStick/src/problems/problems.js
@@ -0,0 +1,442 @@
+const problems = [
+  {
+    id: 1,
+    problem: "Describe the process when user visit a website",
+    type: "network",
+  },
+  {
+    id: 2,
+    problem: "What is the purpose of a DNS request?",
+    type: "network",
+  },
+  {
+    id: 3,
+    problem: "What does TCP ensure during data transmission?",
+    type: "network",
+  },
+  {
+    id: 4,
+    problem: "What role does IP play in the TCP/IP protocol?",
+    type: "network",
+  },
+  {
+    id: 5,
+    problem: "What is the main difference between HTTP and HTTPS?",
+    type: "network",
+  },
+  {
+    id: 6,
+    problem: "Describe the process of an HTTP/HTTPS request and response.",
+    type: "network",
+  },
+  {
+    id: 7,
+    problem: "What is HTML?",
+    type: "html",
+  },
+  {
+    id: 8,
+    problem: "What is the basic structure of an HTML document?",
+    type: "html",
+  },
+  {
+    id: 9,
+    problem:
+      "Explain the elements used in the basic structure of an HTML document.",
+    type: "html",
+  },
+  {
+    id: 10,
+    problem: "Differentiate between inline and block elements in HTML",
+    type: "html",
+  },
+  {
+    id: 11,
+    problem:
+      "What is list in HTML? Explain different types of list available in HTML",
+    type: "html",
+  },
+  {
+    id: 12,
+    problem: "Explain tags in HTML",
+    type: "html",
+  },
+  {
+    id: 13,
+    problem: "What are Semantic elements in HTML?",
+    type: "html",
+  },
+  {
+    id: 14,
+    problem: "What are empty elements in HTML?",
+    type: "html",
+  },
+  {
+    id: 15,
+    problem:
+      "Provide examples of semantic tags and describe their typical use cases.",
+    type: "html",
+  },
+  {
+    id: 16,
+    problem:
+      "Why \u2018alt\u2019 attribute is used with the <img> tag in HTML?",
+    type: "html",
+  },
+  {
+    id: 17,
+    problem: "Why the <meta charset = \u201cUTF-8\u201d> tag is used?",
+    type: "html",
+  },
+  {
+    id: 18,
+    problem:
+      "What is the purpose of using the \u2018role\u2019 attribute in HTML?",
+    type: "html",
+  },
+  {
+    id: 19,
+    problem:
+      "Explain the steps involved in the critical rendering path, from HTML parsing to painting.",
+    type: "html",
+  },
+  {
+    id: 20,
+    problem:
+      "What is the DOM tree, and how is it constructed during HTML parsing?",
+    type: "html",
+  },
+  {
+    id: 21,
+    problem:
+      "Differentiate between the async and defer attributes of the <script> tag. How do they affect script loading and execution?",
+    type: "html",
+  },
+  {
+    id: 22,
+    problem: "What is CSS",
+    type: "css",
+  },
+  {
+    id: 23,
+    problem:
+      "What is the meaning of \u2018Cascading\u2019 in Cascading Style Sheet?",
+    type: "css",
+  },
+  {
+    id: 24,
+    problem: "Explain Media Queries in CSS",
+    type: "css",
+  },
+  {
+    id: 25,
+    problem:
+      "Describe the different types of CSS selectors. Provide examples of each.",
+    type: "css",
+  },
+  {
+    id: 26,
+    problem:
+      "Explain the concept of the cascade in CSS. How does the order of CSS rules affect the styling of elements?",
+    type: "css",
+  },
+  {
+    id: 27,
+    problem: "Explain box-model in CSS",
+    type: "css",
+  },
+  {
+    id: 28,
+    problem: "What is the purpose of z-index property in CSS?",
+    type: "css",
+  },
+  {
+    id: 29,
+    problem:
+      "Explain different ways to display an element at the center of the web page.",
+    type: "css",
+  },
+  {
+    id: 30,
+    problem:
+      "Compare and contrast the content-box and border-box models. How do they affect the calculation of an element\u2019s dimensions?",
+    type: "css",
+  },
+  {
+    id: 31,
+    problem:
+      "How are padding and border included in the width and height calculations of an element in each box model?",
+    type: "css",
+  },
+  {
+    id: 32,
+    problem:
+      "Describe the differences between static, relative, absolute, fixed, and sticky positioning in CSS. Provide examples of when each type would be used.",
+    type: "css",
+  },
+  {
+    id: 33,
+    problem: "What are pseudo classes and pseudo elements in CSS?",
+    type: "css",
+  },
+  {
+    id: 34,
+    problem:
+      "Explain the main concepts of Flexbox and Grid layout systems in CSS. How do they differ in terms of use cases?",
+    type: "css",
+  },
+  {
+    id: 35,
+    problem:
+      "What is the BEM methodology in CSS? How does it help in organizing and maintaining CSS code?",
+    type: "css",
+  },
+  {
+    id: 36,
+    problem:
+      "Explain what it means that JavaScript is an interpreted language. How does this affect the execution of JavaScript code?",
+    type: "javascript",
+  },
+  {
+    id: 37,
+    problem:
+      "Describe how JavaScript uses prototypes for inheritance. Provide an example.",
+    type: "javascript",
+  },
+  {
+    id: 38,
+    problem:
+      "What are first-class functions in JavaScript? How does this feature affect the way functions can be used?",
+    type: "javascript",
+  },
+  {
+    id: 39,
+    problem:
+      "Compare and contrast var, let, and const in terms of scope, hoisting, redeclaration, and reassignment.",
+    type: "javascript",
+  },
+  {
+    id: 40,
+    problem: "What are the rules for naming variables in JavaScript?",
+    type: "javascript",
+  },
+  {
+    id: 41,
+    problem:
+      "List and describe the primitive data types in JavaScript. Provide examples of each.",
+    type: "javascript",
+  },
+  {
+    id: 42,
+    problem:
+      "What are objects in JavaScript? Provide examples of different types of objects.",
+    type: "javascript",
+  },
+  {
+    id: 43,
+    problem:
+      "Explain the use of the alert, prompt, and confirm functions. Provide examples of how each can be used.",
+    type: "javascript",
+  },
+  {
+    id: 44,
+    problem: "Describe null and undefined",
+    type: "javascript",
+  },
+  {
+    id: 45,
+    problem:
+      'How does type conversion work in JavaScript? What are some common "falsy" values?',
+    type: "javascript",
+  },
+  {
+    id: 46,
+    problem:
+      "Describe how the || (logical OR), && (logical AND), and ?? (nullish coalescing) operators work in JavaScript. Provide examples.",
+    type: "javascript",
+  },
+  {
+    id: 47,
+    problem:
+      "Compare function declarations, function expressions, and arrow functions. Provide examples of each.",
+    type: "javascript",
+  },
+  {
+    id: 48,
+    problem:
+      "What are the advantages and disadvantages of using arrow functions?",
+    type: "javascript",
+  },
+  {
+    id: 49,
+    problem:
+      "What is the event loop in JavaScript? How does it handle asynchronous operations?",
+    type: "javascript",
+  },
+  {
+    id: 50,
+    problem:
+      "Explain lexical scoping and the scope chain in JavaScript. Provide examples to illustrate how they work.",
+    type: "javascript",
+  },
+  {
+    id: 51,
+    problem:
+      "What is hoisting in JavaScript? How does it affect the behavior of variables and functions?",
+    type: "javascript",
+  },
+  {
+    id: 52,
+    problem:
+      "Describe how the this keyword works in different contexts: method calls, simple function calls, arrow functions, and event listeners. Provide examples.",
+    type: "javascript",
+  },
+  {
+    id: 53,
+    problem:
+      "How do call, apply, and bind methods affect the value of this in JavaScript functions?",
+    type: "javascript",
+  },
+  {
+    id: 54,
+    problem:
+      "What are some of the key features introduced in ES6 (ECMAScript 2015)?",
+    type: "javascript",
+  },
+  {
+    id: 55,
+    problem: "Different ways to create objects in JS?",
+    type: "javascript",
+  },
+  {
+    id: 56,
+    problem: "Different ways to implement class in JS?",
+    type: "javascript",
+  },
+  {
+    id: 57,
+    problem:
+      "How do classes in ES6 differ from traditional function-based object creation in JavaScript?",
+    type: "javascript",
+  },
+  {
+    id: 58,
+    problem:
+      "What is a closure in JavaScript? How does it work, and why is it useful? Provide an example.",
+    type: "javascript",
+  },
+  {
+    id: 59,
+    problem:
+      "How are objects and prototypes used in JavaScript to achieve inheritance? Provide an example.",
+    type: "javascript",
+  },
+  {
+    id: 60,
+    problem:
+      "Explain the role of the constructor function and class syntax in ES6. How do they facilitate object creation and inheritance?",
+    type: "javascript",
+  },
+  {
+    id: 61,
+    problem:
+      "Describe the concepts of encapsulation, abstraction, and polymorphism in the context of JavaScript. Provide examples of how each can be implemented.",
+    type: "javascript",
+  },
+  {
+    id: 62,
+    problem:
+      "What are rest parameters in JavaScript? Provide an example of how they are used.",
+    type: "javascript",
+  },
+  {
+    id: 63,
+    problem:
+      "Explain how to perform a deep copy of an object in JavaScript. Provide examples using both custom functions and external libraries.",
+    type: "javascript",
+  },
+  {
+    id: 64,
+    problem: "Difference betweein __proto__, [[Prototype]], and prototype",
+    type: "javascript",
+  },
+  {
+    id: 65,
+    problem: "What is strict mode",
+    type: "javascript",
+  },
+  {
+    id: 66,
+    problem: "What are callback functions in JavaScript?",
+    type: "javascript",
+  },
+  {
+    id: 67,
+    problem: "What is callback hell and how to avoid it?",
+    type: "javascript",
+  },
+  {
+    id: 68,
+    problem: "Explain Promises in JavaScript.",
+    type: "javascript",
+  },
+  {
+    id: 69,
+    problem: "How do you handle promises",
+    type: "javascript",
+  },
+  {
+    id: 70,
+    problem: "How do then, catch, and finally methods work with Promises?",
+    type: "javascript",
+  },
+  {
+    id: 71,
+    problem: "What is the use of async await in JavaScript?",
+    type: "javascript",
+  },
+  {
+    id: 72,
+    problem: "Compare then with async await",
+    type: "javascript",
+  },
+  {
+    id: 73,
+    problem: "Explain Promise.all()",
+    type: "javascript",
+  },
+  {
+    id: 74,
+    problem: "Explain Promise.allSettled();",
+    type: "javascript",
+  },
+  {
+    id: 75,
+    problem:
+      "What is the difference between instance methods and static methods?",
+    type: "javascript",
+  },
+  {
+    id: 76,
+    problem: "How does Javascript handle concurrency?",
+    type: "javascript",
+  },
+  {
+    id: 77,
+    problem:
+      "What is async/await? How does it differ from using the promise instance methods?",
+    type: "javascript",
+  },
+  {
+    id: 78,
+    problem: "Can you use await outside of an async function?",
+    type: "javascript",
+  },
+  {
+    id: 79,
+    problem: "What is callback hell and why is it considered a problem?",
+    type: "javascript",
+  },
+];
+
+export { problems };
